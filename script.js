d5e77e38c4a4ab7647b833b468fcc7a539061699
Sonika k <sonikak.1907@gmail.com>
2025-10-16 13:28:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,36 @@ backBtn.onclick = () => showPage('homePage');
 
 function openArticle(id) {
   const article = articles[i];
-  const content = document.getElementById('articles');
+  const content = document.getElementById('articlesContent');
   content.innerHTML = `<h2>${article.title}</h2>${article.content}`;
   showPage('articlePage');
+}
+    
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
